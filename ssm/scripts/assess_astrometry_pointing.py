0ae7d94cceeb15dbbd92bd13b7ab1c21d0f34499
Samuel Flis <samuel.d.flis@gmail.com>
2019-11-13 16:58:55 +0100

    Cleanup astrometry script from memoryleak debugging and commented out code

--- ssm/scripts/assess_astrometry_pointing.py
+++ ssm/scripts/assess_astrometry_pointing.py
@@ -1,92 +1,23 @@
-# import matplotlib.pyplot as plt
 import numpy as np
-import dashi
 import healpy
-
-dashi.visual()
-
 import astropy.units as u
 from astropy.time import Time
-from astropy.coordinates import SkyCoord, AltAz, EarthLocation
-# from ctapipe.coordinates import EngineeringCameraFrame
-# from tqdm.auto import tqdm
+from astropy.coordinates import AltAz, EarthLocation
 
-# from ssdaq.data.io import FrameWriter
 from ssdaq.data import Frame
 from ssm.star_cat.hipparcos import load_hipparcos_cat
-# from CHECLabPy.plotting.camera import CameraImage
 from target_calib import CameraConfiguration
 from ssm.pointing.astrometry import (
         generate_hotspots,
         StarPatternMatch,
     )
-# import objgraph
+
 import zmq
-# import random
-# from pympler import tracker
-# import gc
-# import sys
-from types import FrameType
 
-def print_cycles(objects, outstream=sys.stdout, show_progress=False):
-    """
-    objects:       A list of objects to find cycles in.  It is often useful
-                   to pass in gc.garbage to find the cycles that are
-                   preventing some objects from being garbage collected.
-    outstream:     The stream for output.
-    show_progress: If True, print the number of objects reached as they are
-                   found.
-    """
-    def print_path(path):
-        for i, step in enumerate(path):
-            # next "wraps around"
-            next = path[(i + 1) % len(path)]
 
-            outstream.write("   %s -- " % str(type(step)))
-            if isinstance(step, dict):
-                for key, val in step.items():
-                    if val is next:
-                        outstream.write("[%s]" % repr(key))
-                        break
-                    if key is next:
-                        outstream.write("[key] = %s" % repr(val))
-                        break
-            elif isinstance(step, list):
-                outstream.write("[%d]" % step.index(next))
-            elif isinstance(step, tuple):
-                outstream.write("[%d]" % list(step).index(next))
-            else:
-                outstream.write(repr(step))
-            outstream.write(" ->\n")
-        outstream.write("\n")
 
-    def recurse(obj, start, all, current_path):
-        if show_progress:
-            outstream.write("%d\r" % len(all))
 
-        all[id(obj)] = None
 
-        referents = gc.get_referents(obj)
-        for referent in referents:
-            # If we've found our way back to the start, this is
-            # a cycle, so print it out
-            if referent is start:
-                outstream.write("Cyclic: %r\n" % obj)
-                print_path(current_path)
-
-            # Don't go back through the original list of objects, or
-            # through temporary references to the object, since those
-            # are just an artifact of the cycle detector itself.
-            elif referent is objects or isinstance(referent, FrameType):
-                continue
-
-            # We haven't seen this object before, so recurse
-            elif id(referent) not in all:
-                recurse(referent, start, all, current_path + [obj])
-
-    for obj in objects:
-
-        recurse(obj, obj, { }, [])
 def main(args):
     # tr = tracker.SummaryTracker()
     sdt, stars = load_hipparcos_cat()
@@ -138,26 +69,17 @@ def main(args):
     ip = '127.0.0.1'
     con_str = "tcp://%s:" % ip + str(args.port)
     socket.connect(con_str)
-    # print("*******************************")
-    # objgraph.show_growth(limit=11)
-    # objs = []
+
     obstime = Time(tstamp0+np.random.uniform(-43200,43200),format='unix')
     pixsize = mapping.GetSize()
-    tr.print_diff()
-    print(gc.get_threshold())
     for i in range(args.n_iterations):
         print(f"Sample: {i}", flush=True)
-        # del altaz_frame
-        # del obstime
         obstime = Time(tstamp0+np.random.uniform(-43200,43200),format='unix')
         altaz_frame = AltAz(location=location, obstime=obstime)
-        # alt, az = np.deg2rad(73.21 ), 0#12.1
         pixid = int(np.random.uniform(0,npixs_above_horizon.shape[0]))
         ang = healpy.pix2ang(nside,pixid)
         alt = ang[0]
         az = ang[1]
-        # az = np.random.uniform(0,2*np.pi)
-        # alt = np.arccos(np.random.uniform(0,np.cos(np.pi*alt_min/180.)))
         hotspots, tel_pointing, star_ind, hips_in_fov, all_hips = generate_hotspots(
             alt * u.rad,
             az * u.rad,
@@ -182,42 +104,16 @@ def main(args):
 
         matched_hs = matcher.identify_stars(hotspots, horizon_level=5, obstime=obstime, only_one_it=False)
         if matched_hs is not None and len(matched_hs)>0:
-        # telescope_pointing = SkyCoord(alt=alt * u.rad, az=az * u.rad, frame=altaz_frame)
             ra, dec = matcher.determine_pointing(matched_hs)
             frame.add('matched_hs', np.array(matched_hs))
             frame.add('est_pointing', np.array([ra,dec]))
 
-        # objs += []
-        # print(frame)
         socket.send(frame.serialize())
-        # print("*******************************")
-        # objgraph.show_growth(limit=40)
-        # if i%10==0:
-            # tr.print_diff()
-        # print("Uncollectable garbage:", gc.garbage)
-        # print(gc.get_objects())
-        # print(gc.get_count())
-
-
-        # if i > 2:
-        #     f = open('objects.out','w')
-        #     print_cycles(gc.get_objects(),f)
-        # gc.collect()
-#     objgraph.show_refs(objs+[matcher,stars,source_star_table,altaz_frame,obstime],filename='sample-graph.png')
-#     objgraph.show_backrefs(objs+[matcher,stars,source_star_table],filename='frame-backref-graph.png')
-#     objgraph.show_chain(
-
-#         objgraph.find_backref_chain(
-#             objgraph.by_type('dict')[0],
-#             objgraph.is_proper_module),
-#     filename='chain.png')
-# #
 
 
 
 
 import argparse
-
 from ssdaq.core.basesubscribers import BasicSubscriber,WriterSubscriber
 from ssdaq.data import io
 class FrameSubscriber(BasicSubscriber):
